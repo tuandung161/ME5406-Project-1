cafe2d1bcb8f10adc437829e37919daa5e10f871
Nguyen Tuan Dung <tuandzung1601@gmail.com>
2023-03-10 13:43:47 +0800

    feat: improve graph plotting for all 3 algo

--- env/MonteCarlo.py
+++ env/MonteCarlo.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
+import time
 
 class MonteCarlo():
     def __init__(self, map_size, n_step=1000, n_episode = 10000, epsilon=0.1, gamma=0.9):
@@ -26,6 +27,7 @@ class MonteCarlo():
         
         self.total_reward = []
         self.avg_reward = []
+        self.train_time = 0
 
         #Parameter for test set
         self.test_eps = 0
@@ -91,6 +93,7 @@ class MonteCarlo():
     
     def train(self):
         episode = 0
+        start_time = time.time()
         for i in range(self.n_episode):
             state_action, reward, last_reward = self.run_episode()
             episode += 1
@@ -110,6 +113,10 @@ class MonteCarlo():
             print("Episode ", episode, " completed")
             print("AVG reward: " + str(self.avg_reward[-1]))
             self.episode_count.append(episode)
+        end_time = time.time()
+        self.train_time = round(end_time-start_time,2)
+        print("Train success percentage: ", self.train_success/self.n_episode * 100, "%")
+        print("Train Duration: ", self.train_time, " seconds")
         return self.Q_table
     
     def test(self, test_eps):
@@ -137,41 +144,61 @@ class MonteCarlo():
         print("Test success percentage: ", self.test_sucess/self.test_eps * 100, "%")
 
     def plot(self):
-        fig, axes = plt.subplots(2,3)
-        plt.tight_layout(pad=10)
-        plt.figure(figsize=(60,40))
-
-        #Plot success vs fail count for train set
-        axes[0,0].set_title("[Train] Success and Fail Count")
-        axes[0,0].bar(["Success", "Fail"], height=[int(self.train_success), int(self.train_fail)], align='center', color=["green", "red"])
-        axes[0,0].set(ylabel="Count")
-
-        #Plot success vs fail count for test set
-        axes[1,0].set_title("[Test] Success and Fail Count")
-        axes[1,0].bar(["Success", "Fail"], height=[int(self.test_sucess), int(self.test_fail)], align='center', color=["green", "red"])
-        axes[1,0].set(ylabel="Count")
-
-        #Plot average reward over time for train set
-        axes[0,1].set_title("[Train] Average reward vs episode")
-        axes[0,1].set(xlabel="Episode", ylabel="Average Reward")
-        axes[0,1].plot(self.avg_reward)
-        axes[0,1].yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-
-        #Plot average reward over time for test set
-        axes[1,1].set_title("[Test] Average reward vs episode")
-        axes[1,1].set(xlabel="Episode", ylabel="Average Reward")
-        axes[1,1].plot(self.test_avg_reward)
-        axes[1,1].yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-        
-        #Plot steps per episode for train set
-        axes[0,2].set_title("[Train] Number of step per episode")
-        axes[0,2].scatter(self.episode_count, self.step_count, marker=".")
-        axes[0,2].set(xlabel="Episode", ylabel="Steps")
-        
-        #Plot steps per episode for test set
-        axes[1,2].set_title("[Test] Number of step per episode")
-        axes[1,2].scatter(self.test_episode_count, self.test_step_count, marker=".")
-        axes[1,2].set(xlabel="Episode", ylabel="Steps")
+        terminate = False
+        i = 0
+        while not terminate: 
+            fig, axes = plt.subplots(1,1)
+            i += 1
+            #print_mode = int(input("Print mode: "))
+            print_mode = i
+            #plt.tight_layout(pad=10)
+            #plt.figure(figsize=(60,40))
+            #fig.autoscale(True)
+            if print_mode == 1: 
+                #Plot success vs fail count for train set
+                axes.set_title("[Train] Success and Fail Count")
+                axes.bar(["Success", "Fail"], height=[int(self.train_success), int(self.train_fail)], align='center', color=["green", "red"])
+                axes.set(ylabel="Count")
+                fig.savefig("figure/Monte_Carlo/%sx%s/[Train] Success and Fail Count.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 2: 
+                #Plot success vs fail count for test set
+                axes.set_title("[Test] Success and Fail Count")
+                axes.bar(["Success", "Fail"], height=[int(self.test_sucess), int(self.test_fail)], align='center', color=["green", "red"])
+                axes.set(ylabel="Count")
+                fig.savefig("figure/Monte_Carlo/%sx%s/[Test] Success and Fail Count.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 3:
+                #Plot average reward over time for train set
+                axes.set_title("[Train] Average reward vs episode")
+                axes.set(xlabel="Episode", ylabel="Average Reward")
+                axes.plot(self.avg_reward)
+                axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+                axes.set_ylim(ymax=1,ymin=-1)
+                fig.savefig("figure/Monte_Carlo/%sx%s/[Train] Average reward vs episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 4:
+                #Plot average reward over time for test set
+                axes.set_title("[Test] Average reward vs episode")
+                axes.set(xlabel="Episode", ylabel="Average Reward")
+                axes.plot(self.test_avg_reward)
+                axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+                axes.set_ylim(ymax=1.1,ymin=-1.1)
+                fig.savefig("figure/Monte_Carlo/%sx%s/[Test] Average reward vs episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 5:
+                #Plot steps per episode for train set
+                axes.set_title("[Train] Number of step per episode")
+                axes.scatter(self.episode_count, self.step_count, marker=".")
+                axes.set(xlabel="Episode", ylabel="Steps")
+                axes.set_ylim(ymax=1000,ymin=0)
+                fig.savefig("figure/Monte_Carlo/%sx%s/[Train] Number of step per episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 6:
+                #Plot steps per episode for test set
+                axes.set_title("[Test] Number of step per episode")
+                axes.scatter(self.test_episode_count, self.test_step_count, marker=".")
+                axes.set(xlabel="Episode", ylabel="Steps")
+                axes.set_ylim(ymax=25,ymin=0)
+                fig.savefig("figure/Monte_Carlo/%sx%s/[Test] Number of step per episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 7:
+                print("Plot completed and saved")
+                terminate = True
 
 if __name__ == "__main__": 
     #Initialize & train robot:
--- env/QLearning.py
+++ env/QLearning.py
@@ -1,6 +1,9 @@
 from Environment import Frozen_Lake_Env
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import time
 
 class QLearning():
     def __init__(self, map_size=10, n_episode = 10000, learn_rate=0.1, epsilon=0.1, gamma=0.9):
@@ -11,18 +14,32 @@ class QLearning():
         self.n_observation = self.env.len_x * self.env.len_y
         self.n_action = len(self.env.action)
         self.epsilon = epsilon
-        self.gamma = gamma
-
-        self.n_episode = n_episode
+        self.gamma = gamma     
         self.learn_rate = learn_rate
 
         self.Q_table = self.initialize()
+
+        #Parameter for train set
+        self.n_episode = n_episode
+        self.step_count = []
+        self.episode_count = []
 
         self.train_success = 0
         self.train_fail = 0
         
         self.total_reward = []
-        self.avg_reward = 0
+        self.avg_reward = []
+        self.train_time = 0
+
+        #Parameter for test set
+        self.test_eps = 0
+        self.test_sucess = 0
+        self.test_fail= 0
+        self.test_step_limit = 100
+        self.test_reward = []
+        self.test_avg_reward = []
+        self.test_step_count = []
+        self.test_episode_count = []
 
 
     def initialize(self):
@@ -41,6 +58,8 @@ class QLearning():
             #print("Max index: ", max_index)
             return max_index
 
+    def optimal_policy(self,state):
+        return np.argmax(self.Q_table[state])
 
     def run_episode(self): 
         self.env.reset()
@@ -69,20 +88,114 @@ class QLearning():
                 else: 
                     self.train_fail += 1
                 self.total_reward.append(reward)
+                self.step_count.append(current_step)
                 #print("Reward: " + str(reward))
                 print("Episode Compete: " + str(current_step) + " steps")
                 break
 
     def train(self):
+        start_time = time.time()
         for i in range(self.n_episode):
             self.run_episode()
-            self.avg_reward = sum(self.total_reward)/(i+1)
-            print("AVG reward: " + str(self.avg_reward))
+            self.avg_reward.append(sum(self.total_reward)/(i+1))
+            self.episode_count.append(i+1)
+            print("AVG reward: " + str(self.avg_reward[-1]))
             print("Episode {} completed".format(i))
+        end_time = time.time()
+        self.train_time = round(end_time-start_time,2)
+        print("Train success percentage: ", self.train_success/self.n_episode * 100, "%")
+        print("Train Duration: ", self.train_time, " seconds")
         return self.Q_table
+    
+    def test(self, test_eps):
+        self.test_eps = test_eps
+
+        for episode in range(self.test_eps):
+            self.env.reset()
+            current_step = 0
+            state = self.env.state
+
+            while True:
+                action = self.optimal_policy(state)
+                state, reward, complete = self.env.step(action)
+                current_step += 1
+                if complete or current_step == self.test_step_limit:
+                    if reward <= 0:
+                        self.test_fail += 1
+                    elif reward > 0:
+                        self.test_sucess += 1
+                    self.test_step_count.append(current_step)
+                    break
+            self.test_reward.append(reward)
+            avg_reward = sum(self.test_reward)/(episode+1)
+            self.test_avg_reward.append(avg_reward)
+            self.test_episode_count.append(episode+1)
+        print("Test average reward: ", avg_reward)
+        print("Test success percentage: ", self.test_sucess/self.test_eps * 100, "%")
+
+    def plot(self):
+        terminate = False
+        i = 0
+        while not terminate: 
+            fig, axes = plt.subplots(1,1)
+            i += 1
+            #print_mode = int(input("Print mode: "))
+            print_mode = i
+            #plt.tight_layout(pad=10)
+            #plt.figure(figsize=(60,40))
+            #fig.autoscale(True)
+            if print_mode == 1: 
+                #Plot success vs fail count for train set
+                axes.set_title("[Train] Success and Fail Count")
+                axes.bar(["Success", "Fail"], height=[int(self.train_success), int(self.train_fail)], align='center', color=["green", "red"])
+                axes.set(ylabel="Count")
+                fig.savefig("figure/QLearning/%sx%s/[Train] Success and Fail Count.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 2: 
+                #Plot success vs fail count for test set
+                axes.set_title("[Test] Success and Fail Count")
+                axes.bar(["Success", "Fail"], height=[int(self.test_sucess), int(self.test_fail)], align='center', color=["green", "red"])
+                axes.set(ylabel="Count")
+                fig.savefig("figure/QLearning/%sx%s/[Test] Success and Fail Count.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 3:
+                #Plot average reward over time for train set
+                axes.set_title("[Train] Average reward vs episode")
+                axes.set(xlabel="Episode", ylabel="Average Reward")
+                axes.plot(self.avg_reward)
+                axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+                axes.set_ylim(ymax=1,ymin=-1)
+                fig.savefig("figure/QLearning/%sx%s/[Train] Average reward vs episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 4:
+                #Plot average reward over time for test set
+                axes.set_title("[Test] Average reward vs episode")
+                axes.set(xlabel="Episode", ylabel="Average Reward")
+                axes.plot(self.test_avg_reward)
+                axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+                axes.set_ylim(ymax=1.1,ymin=-1.1)
+                fig.savefig("figure/QLearning/%sx%s/[Test] Average reward vs episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 5:
+                #Plot steps per episode for train set
+                axes.set_title("[Train] Number of step per episode")
+                axes.scatter(self.episode_count, self.step_count, marker=".")
+                axes.set(xlabel="Episode", ylabel="Steps")
+                axes.set_ylim(ymax=250,ymin=0)
+                fig.savefig("figure/QLearning/%sx%s/[Train] Number of step per episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 6:
+                #Plot steps per episode for test set
+                axes.set_title("[Test] Number of step per episode")
+                axes.scatter(self.test_episode_count, self.test_step_count, marker=".")
+                axes.set(xlabel="Episode", ylabel="Steps")
+                axes.set_ylim(ymax=25,ymin=0)
+                fig.savefig("figure/QLearning/%sx%s/[Test] Number of step per episode.png"%(str(self.map_size),str(self.map_size)))
+            elif print_mode == 7:
+                print("Plot completed and saved")
+                terminate = True
+
 
 if __name__ == "__main__":
-    robot = QLearning()
+    map_size = int(input("Input Map Size: "))
+    robot = QLearning(map_size=map_size)
     Q = robot.train()
+    robot.test(1000)
+    robot.plot()
 
 
